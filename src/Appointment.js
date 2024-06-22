76f3f736d863e25f3ddcf95884130239d3cd0d56
Jamal Hansen <jamal.hansen@gmail.com>
2024-06-22 18:20:58 -0500

    Added AppointmentsDayView component

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,3 +1,20 @@
-export const Appointment = ({customer}) => {
-    return <div>{customer.firstName}</div>
-}
+const appointmentTimeOfDay = (startsAt) => {
+  const [h, m] = new Date(startsAt).toTimeString().split(":");
+  return `${h}:${m}`;
+};
+
+export const Appointment = ({ customer }) => {
+  return <div>{customer.firstName}</div>;
+};
+
+export const AppointmentsDayView = ({ appointments }) => (
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map((appointment) => (
+        <li key={appointment.startsAt}>
+          {appointmentTimeOfDay(appointment.startsAt)}
+        </li>
+      ))}
+    </ol>
+  </div>
+);
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,33 +1,75 @@
-import React, { act } from 'react';
+import React, { act } from "react";
 import ReactDOM from "react-dom/client";
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
-window.React = React
+window.React = React;
 describe("Appointment", () => {
-    let container; 
+  let container;
 
-    beforeEach(() => {
-        container = document.createElement("div");
-        document.body.replaceChildren(container);
-    })
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
 
-    const render = component => {
-        act(() => 
-            ReactDOM.createRoot(container).render(component)
-        );
-    }
+  const render = (component) => {
+    act(() => ReactDOM.createRoot(container).render(component));
+  };
 
-    it("renders the customer first name", () => {
-        const customer = { firstName: "Ashley" }
-        render(<Appointment customer={customer} />)
-        
-        expect(document.body.textContent).toContain("Ashley");
-    })
+  it("renders the customer first name", () => {
+    const customer = { firstName: "Ashley" };
+    render(<Appointment customer={customer} />);
 
-    it("renders another customer first name", () => {
-        const customer = { firstName: "Jordan" }
-        render(<Appointment customer={customer} />)
+    expect(document.body.textContent).toContain("Ashley");
+  });
 
-        expect(document.body.textContent).toContain("Jordan");
-    })
-})
+  it("renders another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    render(<Appointment customer={customer} />);
+
+    expect(document.body.textContent).toContain("Jordan");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) => {
+    act(() => ReactDOM.createRoot(container).render(component));
+  };
+
+  const today = new Date();
+  const twoAppointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) },
+  ];
+
+  it("renders with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+
+  it("renders an ol element to display appointments", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    const listElement = document.querySelector("ol");
+    expect(listElement).not.toBeNull();
+  });
+
+  it("renders an li element for each appointment", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    const listChildren = document.querySelectorAll("ol > li");
+    expect(listChildren).toHaveLength(2);
+  });
+
+  it("renders the time of each appointment", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    const listChildren = document.querySelectorAll("ol > li");
+    expect(listChildren[0].textContent).toEqual("12:00");
+    expect(listChildren[1].textContent).toEqual("13:00");
+  });
+});
