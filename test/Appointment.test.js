5a622b0043780f2a18dc29d95416e3abd09cfb80
Jamal Hansen <jamal.hansen@gmail.com>
2024-06-22 17:38:45 -0500

    Finished Chapter 1

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -10,26 +10,24 @@ describe("Appointment", () => {
         container = document.createElement("div");
         document.body.replaceChildren(container);
     })
-    it("renders the customer first name", () => {
-        const customer = { firstName: "Ashley" }
-        const component = (
-            <Appointment customer={customer} />
-        )
-        
+
+    const render = component => {
         act(() => 
             ReactDOM.createRoot(container).render(component)
         );
+    }
+
+    it("renders the customer first name", () => {
+        const customer = { firstName: "Ashley" }
+        render(<Appointment customer={customer} />)
+        
         expect(document.body.textContent).toContain("Ashley");
     })
 
     it("renders another customer first name", () => {
         const customer = { firstName: "Jordan" }
-        const component = (
-            <Appointment customer={customer} />
-        )
-        act(() => 
-            ReactDOM.createRoot(container).render(component)
-        );
+        render(<Appointment customer={customer} />)
+
         expect(document.body.textContent).toContain("Jordan");
     })
 })
